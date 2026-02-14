31586ad1243775e306f7b41f1486da315f865c79
Luidson Lima Santos <luidsonlsti@gmail.com>
2026-02-14 18:43:37 -0300

    start spring project

---
+++ inventory-manager-api/src/main/java/com/github/luidsonl/inventory_manager_api/InventoryManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.luidsonl.inventory_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagerApiApplication.class, args);
+	}
+
+}
---
+++ inventory-manager-api/src/test/java/com/github/luidsonl/inventory_manager_api/InventoryManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.luidsonl.inventory_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
